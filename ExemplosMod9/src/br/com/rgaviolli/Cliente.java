f1c7fec5782819cc885d41a1fa8735257472e2c2
Rafael Gaviolli <bygamerrcg@hotmail.com>
2022-06-20 17:52:37 -0300

    folder

---
+++ ExemplosMod9/src/br/com/rgaviolli/Cliente.java
@@ -0,0 +1,30 @@
+package br.com.rgaviolli;
+
+public class Cliente {
+
+    private Integer age;
+
+    private Long code;
+
+    private Long biggestCode=123456l;
+
+    private Double dexValue=10.1;
+
+    private Boolean status = false;
+
+    private Float dexValue1=10.0f;
+
+    private Short shor;
+
+    private Byte bi;
+
+    private Character word = 'B';
+
+    public static void main(String args[]){
+
+    }
+
+    public void test(){
+
+    }
+}
